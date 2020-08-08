b88e369cbb22757a10bce44733c3f91d8c4c0f08
ryan <sally41176789@yahoo.com.tw>
2020-08-09 01:16:54 +0800

    fix about-page

--- src/views/frontend/Detail.vue
+++ src/views/frontend/Detail.vue
@@ -41,12 +41,43 @@
           </div>
         </div>
       </div>
+      <h5 class="text-danger mt-3 pt-5 pb-3 ml-minus">買了此商品的人，也買了...</h5>
+      <div class="row">
+        <div class="col-md-3 px-2 pb-4" v-for="item in randomFour" :key="item.id">
+          <a href="#" class="image-wrap" @click.prevent="toDetail(item.id)">
+            <div class="rectangle-image bg-cover"
+            :style="{backgroundImage: `url(${item.imageUrl})`}"></div>
+          </a>
+          <a href="#" class="detail-name" @click.prevent="toDetail(item.id)">
+            {{ item.title }}
+          </a>
+          <div class="pt-1">
+            <span class="detail-price">特價 {{ item.price }}</span>
+            <del class="detail-origin-price">原價 {{ item.origin_price }}</del>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-8 ls-2 lh-30">
+          <h5 class="text-danger ml-minus py-4">購物說明</h5>
+          <p class="text-white">若您具有法人身份為常態性且大量購買者，或有特殊作業需求，建議您可洽詢「企業採購」。</p>
+          <p class="text-about pt-2">退換貨說明</p>
+          <p class="text-white">
+            會員所購買的商品均享有到貨十天的猶豫期（含例假日）。退回之商品必須於猶豫期內寄回。
+            <br>
+            辦理退換貨時，商品必須是全新狀態與完整包裝(請注意保持商品本體、
+            配件、贈品、保證書、 原廠包裝及所有附隨文件或資料的完整性，切勿缺漏任何配件或損毀原廠外盒)。 退回商品無法回復原狀者，恐將影響退貨權益或需負擔部分費用。
+            <br>
+            訂購本商品前請務必詳閱商品退換貨原則。
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
 
 export default {
   name: 'Detail',
@@ -56,6 +87,7 @@ export default {
     };
   },
   methods: {
+    ...mapActions(['getProducts']),
     getProduct(id) {
       this.$store.dispatch('getProductToPage', id);
       this.product_num = 1;
@@ -73,13 +105,21 @@ export default {
         this.$store.dispatch('addToCart', { id, qty });
       }
     },
+    toDetail(id) {
+      this.$router.push(`/detail/${id}`);
+    },
   },
   computed: {
     ...mapGetters(['isLoading', 'products', 'product', 'loadingId', 'carts', 'runGif']),
+    randomFour() {
+      const num = Math.floor(Math.random() * 17);
+      return Object.values(this.products).slice(num, num + 4);
+    },
   },
   created() {
     this.productId = this.$route.params.productId;
     this.getProduct(this.productId);
+    this.getProducts();
   },
   mounted() {
     window.scrollTo(0, 0);
